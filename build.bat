6f6109c2c82eb10846c43cdc7c7a41b76ceac8a1
Kataiser <mecharon1.gm@gmail.com>
2019-04-09 18:53:51 -0500

    Replaced the installer with a build script for releases

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo off
+del /q Iji-TAS-mod.zip
+rd /s /q build_target\
+md build_target
+md build_target\inputs
+md build_target\savestates
+
+xcopy iji_17_tas.exe build_target\ /v /f
+xcopy LICENSE build_target\ /v /f
+xcopy README.md build_target\ /v /f
+xcopy "kill iji.bat" build_target\ /v /f
+xcopy "%~dp0\inputs" "%~dp0\build_target\inputs\" /e /q /v /f
+
+echo.
+echo Building Iji-TAS-mod.zip...
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('build_target', 'Iji_TAS_mod.zip'); }"
+echo Done.
+pause
